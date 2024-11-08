2bd89a435839355b5b045b4afcf1ac4f80d3f0ca
Emre Mesüm <emremesum@gmail.com>
2024-11-09 02:03:59 +0300

    seed data

---
+++ ProductApp.DataAccess/Migrations/20241108225846_SeedDataTable.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ContactForms",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 11, 8, 22, 58, 45, 651, DateTimeKind.Utc).AddTicks(2354), new DateTime(2024, 11, 8, 22, 58, 45, 651, DateTimeKind.Utc).AddTicks(2356) });
+
+            migrationBuilder.UpdateData(
+                table: "ContactForms",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 11, 3, 22, 58, 45, 651, DateTimeKind.Utc).AddTicks(2358), new DateTime(2024, 11, 8, 22, 58, 45, 651, DateTimeKind.Utc).AddTicks(2363) });
+
+            migrationBuilder.UpdateData(
+                table: "ContactForms",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 11, 6, 22, 58, 45, 651, DateTimeKind.Utc).AddTicks(2365), new DateTime(2024, 11, 8, 22, 58, 45, 651, DateTimeKind.Utc).AddTicks(2366) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 11, 8, 22, 58, 45, 651, DateTimeKind.Utc).AddTicks(2395));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 11, 8, 22, 58, 45, 651, DateTimeKind.Utc).AddTicks(2398));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UpdatedDate",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ContactForms",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 11, 8, 22, 49, 49, 658, DateTimeKind.Utc).AddTicks(3247), new DateTime(2024, 11, 8, 22, 49, 49, 658, DateTimeKind.Utc).AddTicks(3248) });
+
+            migrationBuilder.UpdateData(
+                table: "ContactForms",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 11, 3, 22, 49, 49, 658, DateTimeKind.Utc).AddTicks(3250), new DateTime(2024, 11, 8, 22, 49, 49, 658, DateTimeKind.Utc).AddTicks(3256) });
+
+            migrationBuilder.UpdateData(
+                table: "ContactForms",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "UpdatedDate" },
+                values: new object[] { new DateTime(2024, 11, 6, 22, 49, 49, 658, DateTimeKind.Utc).AddTicks(3258), new DateTime(2024, 11, 8, 22, 49, 49, 658, DateTimeKind.Utc).AddTicks(3258) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 11, 8, 22, 49, 49, 658, DateTimeKind.Utc).AddTicks(3295));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 11, 8, 22, 49, 49, 658, DateTimeKind.Utc).AddTicks(3298));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UpdatedDate",
+                value: new DateTime(2024, 11, 8, 22, 49, 49, 658, DateTimeKind.Utc).AddTicks(3074));
+        }
+    }
+}
--- ProductApp.DataAccess/ProductAppDbContext.cs
+++ ProductApp.DataAccess/ProductAppDbContext.cs
@@ -6,7 +6,7 @@ using ProductApp.DataAccess.SupportForm;
 
 namespace ProductApp.DataAccess;
 
-public class ProductAppDbContext:DbContext
+public class ProductAppDbContext : DbContext
 {
     public ProductAppDbContext(DbContextOptions<ProductAppDbContext> options) : base(options)
     {
@@ -14,13 +14,86 @@ public class ProductAppDbContext:DbContext
     }
     public DbSet<Product> Products { get; set; } = default!;
     public DbSet<UserApp> Users { get; set; } = default!;
-	public DbSet<ContactForm> ContactForms { get; set; } = default!;
+    public DbSet<ContactForm> ContactForms { get; set; } = default!;
 
 
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.Entity<UserApp>().HasData(new UserApp
+        {
+            Id = 1,
+            Username = "Admin",
+            Password = "Admin",
+            Role = "Admin",
+            IsDeleted = false
+        });
+
+        // ContactForm tablosu için örnek veriler
+        modelBuilder.Entity<ContactForm>().HasData(
+     new ContactForm
+     {
+         Id = 1,
+         UserId = 1,
+         Subject = "Ürün Sorgusu",
+         Message = "X ürünü mevcut mu?",
+         Status = "Beklemede",
+         CreatedDate = DateTime.UtcNow,
+         UpdatedDate = DateTime.UtcNow,
+         IsDeleted = false
+     },
+     new ContactForm
+     {
+         Id = 2, // Farklı Id atanmış
+         UserId = 1,
+         Subject = "Sipariş Durumu",
+         Message = "1234 numaralı siparişin durumu?",
+         Status = "Çözüldü",
+         CreatedDate = DateTime.UtcNow.AddDays(-5),
+         UpdatedDate = DateTime.UtcNow,
+         IsDeleted = false
+     },
+     new ContactForm
+     {
+         Id = 3, // Farklı Id atanmış
+         UserId = 1,
+         Subject = "Teknik Destek",
+         Message = "Ürün kurulumunda yardıma ihtiyacım var.",
+         Status = "Devam Ediyor",
+         CreatedDate = DateTime.UtcNow.AddDays(-2),
+         UpdatedDate = DateTime.UtcNow,
+         IsDeleted = false
+     }
+ );
+
+        // Product tablosu için örnek veriler
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id=1,
+                Code = "P001",
+                ProductName = "Kablosuz Klavye",
+                Price = 29.99m,
+                CreatedDate = DateTime.UtcNow,
+                ImageUrl = "https://example.com/images/keyboard.png",
+                IsDeleted = false
+            },
+            new Product
+            {
+                Id = 2,
+                Code = "P002",
+                ProductName = "Bluetooth Mouse",
+                Price = 19.99m,
+                CreatedDate = DateTime.UtcNow,
+                ImageUrl = "https://example.com/images/mouse.png",
+                IsDeleted = false
+            }
+        );
+
+
+
+
 
         base.OnModelCreating(modelBuilder);
     }
